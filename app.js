523e111e06149070482ebfcb3183f2563e406cea
soham pingat <soham.pingat@gmail.com>
2020-11-24 12:10:22 +0530

    Final Project with get and set operations

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require('ejs');
 const app = express();
 const serviceAccount = require('./key.json');
 
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
@@ -21,21 +22,21 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-app.get('/',function(req,res){
+//app.get('/',function(req,res){
     
-    // const snapshot = db.collection('test2').get()
-    // .then(snapshot=>{
-    //     snapshot.docs.forEach(elem=>{
+//     const snapshot = db.collection('test2').get()
+//     .then(snapshot=>{
+//         snapshot.docs.forEach(elem=>{
             
-    //         if(elem.data().City=='Pune'){
-    //             console.log("IN Pune BllodBanks:- "+ elem.data().Name)
-    //         }
-    //     })
-   // });
+//             if(elem.data().City=='Pune'){
+//                 console.log("IN Pune BllodBanks:- "+ elem.data().Name)
+//             }
+//         })
+//    });
   
-    res.render("index")
+    //res.render("index")
 
-});
+//});
 // app.post('/',function(req,res){
 //     var a = req.body.firstname;
 //     var b = req.body.city;
@@ -60,27 +61,82 @@ app.get('/',function(req,res){
 app.route('/')
             .get(function(req,res){
                 res.render("index")
-            })
-            .post(function(req,res){
-                var name = req.body.fname;
-                var city = req.body.city;
-                console.log(name + city);
-                const docRef = db.collection('test').doc('1');
-                docRef.set({
-                    Name: name,
-                    BBCity : city,
-                    Message: "I want to break free"
-                }) 
-                .then(function(){
-                    console.log('Zala re!!')
-
-                })
-                .catch(function(err){
-                    console.log("Error is :- "+err)
-
-                });  
-                res.redirect('/')
             });
+
+var rname;
+var rage;
+var rcity;
+var remail;
+
+app.route('/requester')
+            .post(function(req,res){
+               rname  = req.body.rname;
+                rage   = req.body.rage;
+                rcity  = req.body.rcity;
+                remail = req.body.remail;
+               res.redirect('/requesterbblist')
+    
+});
+
+app.route('/requesterbblist')
+            .get(function(req,res){
+                var i=0;
+                var bbnamearray=[];
+                var bbnoarray=[];
+                var bbaddarray=[];
+
+
+                const snapshot = db.collection('Bloodbanks').get()
+                    .then(snapshot=>{
+                        snapshot.docs.forEach(elem=>{
+                            
+                            if(elem.data().city=='Pune'){
+                                i++;
+                                var currName= elem.data().Name;
+                                var currNo= elem.data().number;
+                                var currAdd= elem.data().Address;
+                               // console.log(currName)
+                                bbnamearray.push(currName);
+                                bbnoarray.push(currNo);
+                                bbaddarray.push(currAdd);
+                                // console.log("IN Pune BllodBanks:- "+ elem.data().Name+' '+i)
+                                
+                            }
+                        })
+                        console.log(" "+bbnamearray);
+                        console.log(" "+bbnoarray);
+                        console.log(" "+bbaddarray);
+                        res.render('rbblist',{
+                            i:i,
+                            BBnamearray : bbnamearray,
+                            BBnoarray:bbnoarray,
+                            BBaddarray:bbaddarray
+
+                        })
+                }); 
+ });
+
+ app.post('/donor',function(req,res){
+     var dname = req.body.dname;
+     var dage = req.body.dage;
+     var demail = req.body.demail;
+     var ref = db.collection('Donors').doc(dname);
+
+     ref.set({
+         Name : dname,
+         Age : dage,
+         Email : demail
+     })
+     .then(function(){
+
+        res.write('<script>window.alert("Your Request has been recorded and your information is stored in our database" );window.location="/";</script>');
+          res.redirect('/')
+     })
+        .catch(function(err){
+           console.log("ERROR IS :- " + err);
+     })
+     
+ })
 
 app.listen(process.env.PORT||3000,function(){
     console.log("Server started on port 3000");
